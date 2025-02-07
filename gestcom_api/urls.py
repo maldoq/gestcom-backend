47a545a614ed9799de85cfeab8c33ea4e5e3ae43
maldoq <dominiquemalik20@gmail.com>
2025-02-07 09:49:53 +0000

    fix urls importation

--- gestcom_api/urls.py
+++ gestcom_api/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from .views import RegisterView, LoginView, ProfileView
+from .views import RegisterView, LoginView, ProfileView, CreateBoutiqueView, ListBoutiquesView, UpdateBoutiqueView, DeleteBoutiqueView, UpdateModesPaiementView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('profile/', ProfileView.as_view(), name='profile'),
-    path('boutique/creer/', CreateBoutiqueView.as_view(), name='creer-boutique'),
-    path('boutiques/', ListBoutiquesView.as_view(), name='liste-boutiques'),
-    path('boutique/modifier/<int:boutique_id>/', UpdateBoutiqueView.as_view(), name='modifier-boutique'),
-    path('boutique/supprimer/<int:boutique_id>/', DeleteBoutiqueView.as_view(), name='supprimer-boutique'),
-    path('boutique/modes-paiement/<int:boutique_id>/', UpdateModesPaiementView.as_view(), name='modes-paiement-boutique'),
+    path('boutique/creer/', CreateBoutiqueView.as_view(), name='creer_boutique'),
+    path('boutiques/', ListBoutiquesView.as_view(), name='liste_boutiques'),
+    path('boutique/modifier/<int:boutique_id>/', UpdateBoutiqueView.as_view(), name='modifier_boutique'),
+    path('boutique/supprimer/<int:boutique_id>/', DeleteBoutiqueView.as_view(), name='supprimer_boutique'),
+    path('boutique/modes-paiement/<int:boutique_id>/', UpdateModesPaiementView.as_view(), name='modes_paiement_boutique'),
 ]
